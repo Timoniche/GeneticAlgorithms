10a5c107cc5711999ce8d5b58b4fc75c82cad429
timoniche <dmitriidulaev@gmail.com>
2024-02-10 01:37:25 +0300

    FloodFill image generator

--- FloodFillHW1/src/main/java/com/ddulaev/FloodFill.java
+++ FloodFillHW1/src/main/java/com/ddulaev/FloodFill.java
@@ -1,7 +1,47 @@
 package com.ddulaev;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class FloodFill {
+    private final List<List<Integer>> image;
+    private final Random random;
 
+    public FloodFill(int imageSize, int colorRange) {
+        random = new Random();
+        this.image = randomImage(imageSize, colorRange);
+    }
 
-    
+    public List<List<Integer>> randomImage(int imageSize, int colorRange) {
+        List<List<Integer>> image = new ArrayList<>();
+        for (int i = 0; i < imageSize; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < imageSize; j++) {
+                row.add(random.nextInt(colorRange));
+            }
+            image.add(row);
+        }
+        return image;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        int sz = image.size();
+        for (List<Integer> rows : image) {
+            for (int j = 0; j < sz; j++) {
+                sb.append(rows.get(j));
+            }
+            sb.append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        int imageSize = 5;
+        int colorRange = 2;
+        FloodFill floodFill = new FloodFill(imageSize, colorRange);
+        System.out.println(floodFill);
+    }
 }
---
+++ FloodFillHW1/src/main/java/com/ddulaev/Point.java
@@ -0,0 +1,3 @@
+package com.ddulaev;
+
+record Point(int x, int y) {}
