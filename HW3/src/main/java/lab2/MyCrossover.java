dd25b8d28ee280da78a7157c401787ea25d4b318
timoniche <dmitriidulaev@gmail.com>
2024-02-13 13:48:10 +0300

    HW3: mutation implementation

--- HW3/src/main/java/lab2/MyCrossover.java
+++ HW3/src/main/java/lab2/MyCrossover.java
@@ -12,12 +12,29 @@ public class MyCrossover extends AbstractCrossover<double[]> {
     }
 
     protected List<double[]> mate(double[] p1, double[] p2, int i, Random random) {
-        ArrayList children = new ArrayList();
+        ArrayList<double[]> children = new ArrayList<>();
 
         // your implementation:
+        double alpha = 0.3;
+        double[] child1 = arithmeticCrossover(p1, p2, alpha);
+        double[] child2 = arithmeticCrossover(p2, p1, alpha);
+        children.add(child1);
+        children.add(child2);
 
-//        children.add(p1);
-//        children.add(p2);
         return children;
+    }
+
+    private double[] arithmeticCrossover(double[] p, double[] q, double alpha) {
+        if (p.length != q.length) {
+            throw new IllegalArgumentException("Genomes p, q must be the same length");
+        }
+        if (alpha < 0 || alpha > 1) {
+            throw new IllegalArgumentException("Alpha must be in [0, 1]");
+        }
+        double[] child = new double[p.length];
+        for (int i = 0; i < p.length; i++) {
+            child[i] = alpha * p[i] + (1 - alpha) * q[i];
+        }
+        return child;
     }
 }
--- HW3/src/main/java/lab2/MyMutation.java
+++ HW3/src/main/java/lab2/MyMutation.java
@@ -2,8 +2,13 @@ package lab2;
 
 import org.uncommons.watchmaker.framework.EvolutionaryOperator;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import static lab2.MyFactory.MAX_X_DEFINITION;
+import static lab2.MyFactory.MIN_X_DEFINITION;
+import static lab2.MyFactory.generateDoubleBetween;
 
 public class MyMutation implements EvolutionaryOperator<double[]> {
     public List<double[]> apply(List<double[]> population, Random random) {
@@ -12,7 +17,64 @@ public class MyMutation implements EvolutionaryOperator<double[]> {
 
         // your implementation:
 
+        double prob = random.nextDouble();
+        double explorationThreshold = 0.1;
+        if (prob <= explorationThreshold) {
+            return uniformExplorationMutation(population, random);
+        }
+
         //result population
-        return population;
+        return gaussianExploitationMutation(population, random);
     }
+
+    private List<double[]> uniformExplorationMutation(
+            List<double[]> population,
+            Random random
+    ) {
+        List<double[]> mutatedPopulation = new ArrayList<>();
+
+        for (double[] individual : population) {
+            double[] newFeatures = individual.clone();
+
+            for (int i = 0; i < individual.length; i++) {
+                if (random.nextDouble() <= 1. / individual.length) {
+                    newFeatures[i] = generateDoubleBetween(MIN_X_DEFINITION, random, MAX_X_DEFINITION);
+                }
+            }
+
+            mutatedPopulation.add(newFeatures);
+        }
+
+        return mutatedPopulation;
+    }
+
+    private List<double[]> gaussianExploitationMutation(
+            List<double[]> population,
+            Random random
+    ) {
+        List<double[]> mutatedPopulation = new ArrayList<>();
+
+        // https://en.wikipedia.org/wiki/Mutation_(genetic_algorithm)
+        double desiredStandardDeviation = (MAX_X_DEFINITION - MIN_X_DEFINITION) / 6;
+        double desiredMean = 0.;
+
+        for (double[] individual : population) {
+            double[] newFeatures = individual.clone();
+
+            for (int i = 0; i < individual.length; i++) {
+                if (random.nextDouble() <= 1. / individual.length) {
+                    newFeatures[i] = newFeatures[i] + generateGaussian(desiredStandardDeviation, random, desiredMean);
+                }
+            }
+
+            mutatedPopulation.add(newFeatures);
+        }
+
+        return mutatedPopulation;
+    }
+
+    private double generateGaussian(double standardDeviation, Random random, double mean) {
+        return random.nextGaussian() * standardDeviation + mean;
+    }
+
 }
