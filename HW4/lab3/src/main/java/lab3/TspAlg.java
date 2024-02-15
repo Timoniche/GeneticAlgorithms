f87b8978fa5c320e40db60be23929a10d494d36d
timoniche <dmitriidulaev@gmail.com>
2024-02-15 22:54:49 +0300

    HW4: mean run10 impl

--- HW4/lab3/src/main/java/lab3/TspAlg.java
+++ HW4/lab3/src/main/java/lab3/TspAlg.java
@@ -3,7 +3,6 @@ package lab3;
 import org.uncommons.maths.random.Probability;
 import org.uncommons.watchmaker.framework.*;
 import org.uncommons.watchmaker.framework.operators.EvolutionPipeline;
-import org.uncommons.watchmaker.framework.selection.RouletteWheelSelection;
 import org.uncommons.watchmaker.framework.selection.TournamentSelection;
 import org.uncommons.watchmaker.framework.termination.GenerationCount;
 
@@ -17,7 +16,49 @@ public class TspAlg {
     public static int POPULATION_SIZE = 10; //initial 10
     public static int GENERATIONS = 10000; //initial 10
 
+    private static class RunBestDistance {
+        private double bestDistance;
+
+        public RunBestDistance(double bestDistance) {
+            this.bestDistance = bestDistance;
+        }
+
+        public double getBestDistance() {
+            return bestDistance;
+        }
+
+        public void updateBestDistanceIfNeeded(double curDistance) {
+            if (curDistance < bestDistance) {
+                bestDistance = curDistance;
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        run10();
+    }
+
+    public static void run10() {
+        List<Double> distances = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            RunBestDistance runBestDistance = new RunBestDistance(1e8);
+            run(runBestDistance);
+            distances.add(runBestDistance.getBestDistance());
+        }
+        System.out.println("Fits from 10 runs");
+        System.out.println(distances);
+        System.out.println("Mean: " + mean(distances));
+    }
+
+    public static double mean(List<Double> distances) {
+        double sum = 0;
+        for (double distance : distances) {
+            sum += distance;
+        }
+        return sum / distances.size();
+    }
+
+    public static void run(RunBestDistance runBestDistance) {
         String problem = "xqf131.tsp"; // name of problem or path to input file
         TspReader tspReader = new TspReader(problem);
 
@@ -53,6 +94,7 @@ public class TspAlg {
                 double bestFit = populationData.getBestCandidateFitness();
 
                 mutation.setGenerationNumber(populationData.getGenerationNumber() + 1);
+                runBestDistance.updateBestDistanceIfNeeded(bestFit);
 
                 System.out.println("Generation " + populationData.getGenerationNumber() + ": " + bestFit);
                 TspSolution best = (TspSolution) populationData.getBestCandidate();
